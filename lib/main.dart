dc9240dac7ae67e5d025087412bfb9544beb1dbc
welcomeworld <guduziyousiwang@gmail.com>
2024-07-26 08:48:16 +0800

    complete Initialize Firebase in your app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  _initFirebase();
   runApp(const MyApp());
+}
+
+void _initFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
